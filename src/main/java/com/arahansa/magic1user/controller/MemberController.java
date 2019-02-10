ca3ad946e14dabf318ac427e2f28adb754e9d3cd
lucas.choi <lucas.choi@mcngamehub.com>
2019-02-10 14:35:10 +0900

    3 - 리다이렉션과 리스트에 넣기 처리

--- src/main/java/com/arahansa/magic1user/controller/MemberController.java
+++ src/main/java/com/arahansa/magic1user/controller/MemberController.java
@@ -1,5 +1,7 @@
 package com.arahansa.magic1user.controller;
 
+import com.arahansa.magic1user.domain.Member;
+import com.sun.org.apache.xpath.internal.operations.Mod;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -7,20 +9,27 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Slf4j
 @Controller
 @RequestMapping("/member")
 public class MemberController {
 
+    List<Member> memberList = new ArrayList<>();
+
     @GetMapping
-    public String form(){
+    public String form(Model model){
         log.debug("member page..");
+        model.addAttribute("list", memberList);
         return "member/member_create";
     }
 
     @PostMapping
-    public String create(String username, String password, Model model){
-        log.debug("username : {}, password", username, password);
-        return "member/member_create";
+    public String create(Member member, Model model){
+        log.debug("member : {}", member);
+        memberList.add(member);
+        return "redirect:/member";
     }
 }
---
+++ src/main/java/com/arahansa/magic1user/domain/Member.java
@@ -0,0 +1,12 @@
+package com.arahansa.magic1user.domain;
+
+
+import lombok.Data;
+
+@Data
+public class Member {
+
+    private String username;
+    private String password;
+
+}
