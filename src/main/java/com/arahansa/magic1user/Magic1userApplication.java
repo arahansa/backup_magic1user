efe9a56286a24b1dae5d6061bd9b2a12711892a1
lucas.choi <lucas.choi@mcngamehub.com>
2019-02-10 10:36:20 +0900

    first commit

---
+++ src/main/java/com/arahansa/magic1user/Magic1userApplication.java
@@ -0,0 +1,14 @@
+package com.arahansa.magic1user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Magic1userApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Magic1userApplication.class, args);
+    }
+
+}
+
