9de654ab1c6fcff21534b5a3b93c091c27178d42
lucas.choi <lucas.choi@mcngamehub.com>
2019-02-10 13:11:13 +0900

    1 추가 - 정적자원과 모델 설정

--- src/main/java/com/arahansa/magic1user/controller/IndexController.java
+++ src/main/java/com/arahansa/magic1user/controller/IndexController.java
@@ -1,16 +1,23 @@
 package com.arahansa.magic1user.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 /**
  * @author Lucas Choi
  */
+@Slf4j
 @Controller
 public class IndexController {
 
     @GetMapping("/")
-    public String index(){
+    public String index(String name, Model model){
+        log.debug("name : {}", name);
+        model.addAttribute("name", name);
         return "index";
     }
+
 }
